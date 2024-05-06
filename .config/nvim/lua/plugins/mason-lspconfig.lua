afda8aca45331dc7b8dcd1d0a8989c48a1160e8e
Mykhailo Sabatura <mishasbtr@gmail.com>
2024-05-06 10:51:22 +0200

    move to mason-lspconfig

--- .config/nvim/lua/plugins/mason-lspconfig.lua
+++ .config/nvim/lua/plugins/mason-lspconfig.lua
@@ -1,10 +1,12 @@
 return {
-  "neovim/nvim-lspconfig",
+  "williamboman/mason-lspconfig.nvim",
   opts = {
     servers = {
       angularls = {},
       astro = {},
       mdx_analyzer = {},
+      cssls = {},
+      cssmodules_ls = {},
     },
   },
 }
