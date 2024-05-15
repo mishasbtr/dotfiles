f433cbdaacb3199bfc003435e7d149e24e3a6d2c
Mykhailo Sabatura <mishasbtr@gmail.com>
2024-05-15 11:26:58 +0200

    setup keybindings for telescope live grep with args

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -8,8 +8,10 @@ return {
       version = "^1.0.0",
     },
   },
-  config = function()
-    require("telescope").load_extension("live_grep_args")
+  config = function(_, opts)
+    local telescope = require("telescope")
+    telescope.setup(opts)
+    telescope.load_extension("live_grep_args")
   end,
   keys = {
     {
@@ -20,4 +22,23 @@ return {
       desc = "Live Grep with Args",
     },
   },
+  opts = function(_, opts)
+    local lga_actions = require("telescope-live-grep-args.actions")
+    opts.extensions = {
+      live_grep_args = {
+        auto_quoting = true, -- enable/disable auto-quoting
+        -- define mappings, e.g.
+        mappings = { -- extend mappings
+          i = {
+            ["<C-k>"] = lga_actions.quote_prompt(),
+            ["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
+          },
+        },
+        -- ... also accepts theme settings, for example:
+        -- theme = "dropdown", -- use dropdown theme
+        -- theme = { }, -- use own theme spec
+        -- layout_config = { mirror=true }, -- mirror preview pane
+      },
+    }
+  end,
 }
