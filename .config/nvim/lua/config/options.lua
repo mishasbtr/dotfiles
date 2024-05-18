58c5d1f6e30016d83f22c7df9d4413d9fbe74223
Mykhailo Sabatura <mishasbtr@gmail.com>
2024-05-18 16:48:19 +0200

    just for the fuck of it yeah

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,8 +2,5 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-local opt = vim.opt
-
-opt.clipboard = ""
-
+vim.opt.clipboard = ""
 vim.g.camelcasemotion_key = "\\"
