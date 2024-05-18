6610830b119db7d4cd3cd5a0f7f2fb9c891f5f3d
Mykhailo Sabatura <mishasbtr@gmail.com>
2024-05-18 20:00:40 +0200

    add polybar module for showing active power profile

---
+++ .config/polybar/active-power-profile.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+power_profile_print() {
+    powerprofilesctl get | while read -r REPLY; do
+      case $REPLY in
+        "performance")
+          printf "󱐋"
+          ;;
+        "balanced")
+          printf ""
+          ;;
+        "power-saver")
+          printf ""
+          ;;
+        *)
+          printf "power profile: $REPLY"
+          ;;
+      esac
+    done
+}
+
+power_profile_print
+
