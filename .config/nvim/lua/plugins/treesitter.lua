a0290fe2c2799970d81ee5b3bdd9f92b32cfd748
Mykhailo Sabatura <mishasbtr@gmail.com>
2024-05-21 12:49:06 +0200

    extend default treesitter langs instead of overwriting

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,12 +1,14 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  opts = {
-    ensure_installed = {
+  opts = function(_, opts)
+    vim.list_extend(opts.ensure_installed, {
       "angular",
       "astro",
+      "cpp",
       "css",
+      "csv",
       "scheme",
       "scss",
-    },
-  },
+    })
+  end,
 }
