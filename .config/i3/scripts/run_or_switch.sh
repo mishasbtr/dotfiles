2e391b52fa4f0e38f6536fbdbd3c203c36c962be
Mykhailo Sabatura <mishasbtr@gmail.com>
2024-06-05 19:09:06 +0200

    improve run_or_switch script to bring scratchpad windows to the front

--- .config/i3/scripts/run_or_switch.sh
+++ .config/i3/scripts/run_or_switch.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 APP_NAME=$1
+APP_WINDOW_INFO=$(wmctrl -xl | grep -i $APP_NAME)
 
-if wmctrl -xl | grep -qi $APP_NAME; then
-    wmctrl -xa $APP_NAME
-else
+if [ -z "$APP_WINDOW_INFO" ]; then
     i3-msg exec $APP_NAME
+    exit 0
 fi
+
+APP_WINDOW_ID=$(echo $APP_WINDOW_INFO | awk '{print $1}')
+i3-msg "[id=$APP_WINDOW_ID] focus"
+
