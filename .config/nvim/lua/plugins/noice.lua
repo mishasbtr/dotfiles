c7d2c7d49f4b22235ffb30e2546ad54c94cf1845
Mykhailo Sabatura <mishasbtr@gmail.com>
2024-06-07 11:58:47 +0200

    use new opts_extend feature of lazy.nvim

---
+++ .config/nvim/lua/plugins/noice.lua
@@ -0,0 +1,11 @@
+return {
+  "folke/noice.nvim",
+  opts_extend = {
+    "presets.lsp_doc_border",
+  },
+  opts = {
+    presets = {
+      lsp_doc_border = true,
+    },
+  },
+}
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,11 +1,14 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  opts = function(_, opts)
-    vim.list_extend(opts.ensure_installed, {
+  opts_extend = {
+    "ensure_installed",
+  },
+  opts = {
+    ensure_installed = {
       "cpp",
       "css",
       "csv",
       "scss",
-    })
-  end,
+    },
+  },
 }
