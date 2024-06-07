762f65bd5e273f54f9fea7097dc8d8e6ac757085
Mykhailo Sabatura <mishasbtr@gmail.com>
2024-06-07 21:09:35 +0200

    steal cowboy util

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -7,3 +7,41 @@ map("n", "<C-S-Up>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
 map("n", "<C-S-Down>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
 map("n", "<C-S-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
 map("n", "<C-S-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
+
+-- TODO: refactor this once I understand lua modules
+-- hol' up cowboy
+local function cowboy()
+  ---@type table?
+  local id
+  local ok = true
+  for _, key in ipairs({ "h", "j", "k", "l", "+", "-" }) do
+    local count = 0
+    local timer = assert(vim.uv.new_timer())
+    local key_map = key
+    vim.keymap.set("n", key, function()
+      if vim.v.count > 0 then
+        count = 0
+      end
+      if count >= 10 and vim.bo.buftype ~= "nofile" then
+        ok, id = pcall(vim.notify, "Hold it Cowboy!", vim.log.levels.WARN, {
+          icon = "🤠",
+          replace = id,
+          keep = function()
+            return count >= 10
+          end,
+        })
+        if not ok then
+          id = nil
+          return key_map
+        end
+      else
+        count = count + 1
+        timer:start(2000, 0, function()
+          count = 0
+        end)
+        return key_map
+      end
+    end, { expr = true, silent = true })
+  end
+end
+cowboy()
