2cd7355e1df44e80fe66bc004d13d80477b34e1a
Mykhailo Sabatura <mishasbtr@gmail.com>
2024-06-08 15:58:23 +0200

    default to gruvbox dark, of course

--- .config/nvim/lua/plugins/gruvbox.lua
+++ .config/nvim/lua/plugins/gruvbox.lua
@@ -5,7 +5,7 @@ return {
   opts = function(_, opts)
     opts.context = "hard"
     opts.transparent_mode = true
-    vim.o.background = "light"
-    -- vim.o.background = "dark"
+    -- vim.o.background = "light"
+    vim.o.background = "dark"
   end,
 }
