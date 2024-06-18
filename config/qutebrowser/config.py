f18799c9608b8e12f22a4a490bccc6fb0fc73481
Mykhailo Sabatura <mishasbtr@gmail.com>
2024-06-18 20:11:53 +0200

    fix(qutebrowser): fixes to config

--- config/qutebrowser/config.py
+++ config/qutebrowser/config.py
@@ -17,7 +17,7 @@ c.content.default_encoding = "utf-8"
 c.content.blocking.method = "both"
 c.content.javascript.clipboard = "access"
 c.content.notifications.enabled = True
-c.editor.command = ["alacritty", "nvim", "-e", "exec {line}g{column0}l", "{}"]
+c.editor.command = ["alacritty", "-e", "nvim", "{}"]
 c.fileselect.handler = "external"
 c.fileselect.single_file.command = ["alacritty", "sh", "-c", "xplr > {}"]
 c.fileselect.multiple_files.command = ["alacritty", "sh", "-c", "xplr > {}"]
@@ -26,7 +26,6 @@ c.tabs.show = "multiple"
 c.tabs.last_close = "close"
 
 # privacy
-c.content.cookies.accept = "no-3rdparty"
 c.content.webrtc_ip_handling_policy = "default-public-interface-only"
 
 # urls
