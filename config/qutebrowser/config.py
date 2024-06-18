85d0c03d360d09b3b758ed04483a47998ec18f45
Mykhailo Sabatura <mishasbtr@gmail.com>
2024-06-18 12:29:52 +0200

    feat(qutebrowser): install and setup qutebrowser

---
+++ config/qutebrowser/config.py
@@ -0,0 +1,49 @@
+config.load_autoconfig(True)
+
+# ui
+config.source("gruvbox.py")
+c.colors.webpage.darkmode.enabled = True
+c.completion.shrink = True
+c.completion.use_best_match = True
+c.downloads.remove_finished = 10000
+c.statusbar.widgets = ["progress", "keypress", "url", "history"]
+c.tabs.position = "left"
+c.tabs.title.format = "{index}: {audio}{current_title}"
+c.tabs.title.format_pinned = "{index}: {audio}{current_title}"
+
+# general
+c.auto_save.session = True
+c.content.default_encoding = "utf-8"
+c.content.javascript.clipboard = "access"
+c.content.notifications.enabled = True
+c.editor.command = ["alacritty", "nvim", "-e", "exec {line}g{column0}l", "{}"]
+c.fileselect.handler = "external"
+c.fileselect.single_file.command = ["alacritty", "sh", "-c", "xplr > {}"]
+c.fileselect.multiple_files.command = ["alacritty", "sh", "-c", "xplr > {}"]
+c.downloads.location.prompt = True
+c.input.insert_mode.auto_load = True
+c.tabs.show = "multiple"
+c.tabs.last_close = "close"
+
+# privacy
+c.content.cookies.accept = "no-3rdparty"
+c.content.webrtc_ip_handling_policy = "default-public-interface-only"
+
+# urls
+c.url.searchengines = {
+    "DEFAULT": "https://google.com/search?q={}",
+    "?": "https://google.com/search?q={}",
+}
+c.url.default_page = "~/.config/qutebrowser/blank.html"
+c.url.start_pages = ["~/.config/qutebrowser/blank.html"]
+
+
+# keys
+bindings = {
+    "<Ctrl-Shift-J>": "tab-move +",
+    "<Ctrl-Shift-K>": "tab-move -",
+    "D": "undo",
+}
+
+for key, bind in bindings.items():
+    config.bind(key, bind)
