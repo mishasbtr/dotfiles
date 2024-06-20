6036d9876cdcdaaddc38b399766ee344ec6df014
Mykhailo Sabatura <mishasbtr@gmail.com>
2024-06-20 17:54:58 +0200

    feat(ncmpcpp): configure switching if ncmpcpp is open

--- config/i3/scripts/run_or_switch.sh
+++ config/i3/scripts/run_or_switch.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
 app_name=$1
+command=$2
+
 app_window_info=$(wmctrl -xl | grep -i "$app_name")
 
 if [ -z "$app_window_info" ]; then
-	i3-msg exec "$app_name"
-	exit 0
+	if [ -n "$command" ]; then
+		$command
+		exit 0
+	else
+		i3-msg exec "$app_name"
+		exit 0
+	fi
 fi
 
 "$(dirname "$0")"/switch_to_last_of_app.sh "$app_name"
