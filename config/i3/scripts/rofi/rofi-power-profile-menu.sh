1f5ae243a8e440668b7c2d853bb1fa2f24a48817
Mykhailo Sabatura <mishasbtr@gmail.com>
2024-06-23 10:57:02 +0200

    refactor(i3-scripts): make more stuff readonly

--- config/i3/scripts/rofi/rofi-power-profile-menu.sh
+++ config/i3/scripts/rofi/rofi-power-profile-menu.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-performance_menu_option="󱐋 performance"
-balanced_menu_option=" balanced"
-power_saver_menu_option=" power saver"
+readonly performance_menu_option="󱐋 performance"
+readonly balanced_menu_option=" balanced"
+readonly power_saver_menu_option=" power saver"
 
-performance_value="performance"
-balanced_value="balanced"
-power_saver_value="power-saver"
+readonly performance_value="performance"
+readonly balanced_value="balanced"
+readonly power_saver_value="power-saver"
 
-profiles="$performance_menu_option\n$balanced_menu_option\n$power_saver_menu_option"
+readonly profiles="$performance_menu_option\n$balanced_menu_option\n$power_saver_menu_option"
 
+readonly chosen
 chosen=$(echo -e "$profiles" | rofi -dmenu -p "Select power profile")
 
 if [ -z "$chosen" ]; then
 	exit 0
 fi
 
-# case switch for chosen profile
 case $chosen in
 "$performance_menu_option")
 	powerprofilesctl set $performance_value
