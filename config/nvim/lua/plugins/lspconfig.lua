b98154bab92187aca8e589d4f1f9834bdb887020
Mykhailo Sabatura <mishasbtr@gmail.com>
2024-06-27 17:26:07 +0200

    fix(ts): relative impors

--- config/nvim/lua/plugins/lspconfig.lua
+++ config/nvim/lua/plugins/lspconfig.lua
@@ -1,9 +1,6 @@
 return {
   "neovim/nvim-lspconfig",
   ---@class PluginLspOpts
-  opts_extend = {
-    "servers.vtsls.settings.typescript.preferences.importModuleSpecifierPreference",
-  },
   opts = {
     ---@type lspconfig.options
     servers = {
