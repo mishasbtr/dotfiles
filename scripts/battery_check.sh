a2aa25f63b045190f99b85491948fe4d6c2dc227
Mykhailo Sabatura <mishasbtr@gmail.com>
2024-09-02 22:13:44 +0200

    refactor(scripts): refactor battery script

--- scripts/battery_check.sh
+++ scripts/battery_check.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-THRESHOLD=20
+threshold=20
 notification_id=1234
 
-BATTERY_LEVEL=$(acpi -b | grep -P -o '[0-9]+(?=%)')
+battery_level=$(acpi -b | grep -P -o '[0-9]+(?=%)')
 
-if [ "$BATTERY_LEVEL" -le "$THRESHOLD" ]; then
-  dunstify -r $notification_id -u critical "Low Battery" "Battery level is at ${BATTERY_LEVEL}%"
+if [ "$battery_level" -le "$threshold" ]; then
+  dunstify -t 5000 -r $notification_id -u critical "Low Battery" "Battery level is at ${battery_level}%"
 fi
