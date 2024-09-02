1d6317dcfb4372df4bafa542af6121209eba806d
Mykhailo Sabatura <mishasbtr@gmail.com>
2024-09-02 22:15:02 +0200

    refactor(scripts): refactor trackpad script

--- scripts/toggle_trackpad.sh
+++ scripts/toggle_trackpad.sh
@@ -3,9 +3,9 @@
 device_id=9
 notification_id=234789
 
-STATE=$(xinput list-props $device_id | grep "Device Enabled" | grep -o "[01]$")
+enabled_flag=$(xinput list-props $device_id | grep "Device Enabled" | grep -o "[01]$")
 
-if [ "$STATE" -eq 1 ]; then
+if [ "$enabled_flag" -eq 1 ]; then
   xinput disable $device_id
   dunstify -r $notification_id "Trackpad Disabled"
 else
