36989fdaa0224574b76f6bfc2cb399a9abbce0e5
Mykhailo Sabatura <mishasbtr@gmail.com>
2024-09-04 20:45:49 +0200

    chore(scripts): brush up power profile selection script

--- config/i3/scripts/rofi/rofi-power-profile-menu.sh
+++ config/i3/scripts/rofi/rofi-power-profile-menu.sh
@@ -10,22 +10,22 @@ readonly power_saver_value="power-saver"
 
 readonly profiles="$performance_menu_option\n$balanced_menu_option\n$power_saver_menu_option"
 
-chosen=$(echo -e "$profiles" | rofi -dmenu -p "Select power profile")
+chosen_profile=$(echo -e "$profiles" | rofi -dmenu -p "Select power profile")
 
-if [ -z "$chosen" ]; then
-	exit 0
+if [ -z "$chosen_profile" ]; then
+  exit 0
 fi
 
-case $chosen in
+case $chosen_profile in
 "$performance_menu_option")
-	powerprofilesctl set $performance_value
-	;;
+  powerprofilesctl set $performance_value
+  ;;
 "$balanced_menu_option")
-	powerprofilesctl set $balanced_value
-	;;
+  powerprofilesctl set $balanced_value
+  ;;
 "$power_saver_menu_option")
-	powerprofilesctl set $power_saver_value
-	;;
+  powerprofilesctl set $power_saver_value
+  ;;
 esac
 
-dunstify -t 2000 -r 282828 -u normal "Power profile switched to $chosen"
+dunstify -t 2000 -r 282828 -u normal "Power profile switched" "$chosen_profile"
