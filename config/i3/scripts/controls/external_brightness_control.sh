84cd0a1c26efbf2e0da6d0a58d285ba585fb9ecc
Mykhailo Sabatura <mishasbtr@gmail.com>
2024-09-04 21:33:50 +0200

    feat(scripts): add lock to external_brightness_control

--- config/i3/scripts/controls/external_brightness_control.sh
+++ config/i3/scripts/controls/external_brightness_control.sh
@@ -1,26 +1,31 @@
 #!/bin/bash
 
+readonly lockfile="/tmp/external_brightness.lock"
 readonly brightness_vcp_code=10
 
 function get_brightness() {
-	ddcutil --display 1 getvcp $brightness_vcp_code | awk '{print $9}' | sed 's/,$//'
+  ddcutil --display 1 getvcp $brightness_vcp_code | awk '{print $9}' | sed 's/,$//'
 }
 
 function set_brightness() {
-	ddcutil --display 1 setvcp $brightness_vcp_code "$1"
+  ddcutil --display 1 setvcp $brightness_vcp_code "$1"
 }
 
 function show_brightness_notif() {
-	~/.config/i3/scripts/utils/show_control_notification.sh "" "$(get_brightness)"
+  ~/.config/i3/scripts/utils/show_control_notification.sh "" "$(get_brightness)"
 }
 
-case $1 in
-get_brightness)
-	get_brightness
-	;;
+(
+  flock -n 9 || exit 0
 
-set_brightness)
-	set_brightness "$2"
-	show_brightness_notif
-	;;
-esac
+  case $1 in
+  get_brightness)
+    get_brightness
+    ;;
+
+  set_brightness)
+    set_brightness "$2"
+    show_brightness_notif
+    ;;
+  esac
+) 9>"$lockfile"
