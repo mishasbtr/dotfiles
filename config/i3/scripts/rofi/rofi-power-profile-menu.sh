ed096eaa01c34a2e113de7f213c8fad60d4c5aff
Mykhailo Sabatura <mishasbtr@gmail.com>
2024-09-10 21:22:12 +0200

    feat(scripts): add indication of currently active profile to profile switching rofi menu

--- config/i3/scripts/rofi/rofi-power-profile-menu.sh
+++ config/i3/scripts/rofi/rofi-power-profile-menu.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
-readonly performance_menu_option="󱐋  performance"
-readonly balanced_menu_option="  balanced"
-readonly power_saver_menu_option="  power saver"
+performance_menu_option="󱐋  performance"
+balanced_menu_option="  balanced"
+power_saver_menu_option="  power saver"
 
 readonly performance_value="performance"
 readonly balanced_value="balanced"
 readonly power_saver_value="power-saver"
+
+# Get the current active profile
+current_profile=$(powerprofilesctl get)
+
+# Highlight the active profile
+if [ "$current_profile" == "$performance_value" ]; then
+  performance_menu_option="* $performance_menu_option"
+elif [ "$current_profile" == "$balanced_value" ]; then
+  balanced_menu_option="* $balanced_menu_option"
+elif [ "$current_profile" == "$power_saver_value" ]; then
+  power_saver_menu_option="* $power_saver_menu_option"
+fi
 
 readonly profiles="$performance_menu_option\n$balanced_menu_option\n$power_saver_menu_option"
 
@@ -15,6 +27,9 @@ chosen_profile=$(echo -e "$profiles" | rofi -dmenu -p "Select power profile")
 if [ -z "$chosen_profile" ]; then
   exit 0
 fi
+
+# Remove the indicator before comparison
+chosen_profile=$(echo "$chosen_profile" | sed 's/^\* //')
 
 case $chosen_profile in
 "$performance_menu_option")
