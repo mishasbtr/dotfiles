42301160410489b894e934df5a706daf30f4dcb6
Mykhailo Sabatura <mishasbtr@gmail.com>
2025-03-03 03:51:21 +0100

    fix(scripts): fix control notification script

--- config/i3/scripts/utils/show_control_notification.sh
+++ config/i3/scripts/utils/show_control_notification.sh
@@ -2,9 +2,8 @@
 
 readonly bar_color="#ebdbb2"
 readonly show_duration=1000
-readonly notification_id=2593
 
 readonly icon=$1
 readonly percentage=$2
 
-dunstify -t $show_duration -r $notification_id -u normal "$icon  $percentage%" -h int:value:"$percentage" -h string:hlcolor:$bar_color
+dunstify -t $show_duration -h string:x-dunst-stack-tag:control-notification -u normal "$icon  $percentage%" -h int:value:"$percentage" -h string:hlcolor:$bar_color
