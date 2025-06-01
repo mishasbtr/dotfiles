dd379933a81312589b5c162da6de7e882c245544
Mykhailo Sabatura <mishasbtr@gmail.com>
2025-06-01 17:23:18 +0200

    config(nvim): keymap

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -8,11 +8,7 @@ map("n", "<C-S-Down>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" }
 map("n", "<C-S-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
 map("n", "<C-S-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
 
-map("n", "<leader>cp", function()
+map("n", "<leader>cP", function()
   vim.fn.setreg("+", vim.fn.expand("%"))
   print("Copied relative path: " .. vim.fn.expand("%"))
 end, { noremap = true, silent = true, desc = "Copy Relative Path of Active File" })
-
-map("n", "<leader>fw", function()
-  LazyVim.pick("files", { default_text = vim.fn.expand("<cword>") })()
-end, { desc = "Find Files (Current word)" })
