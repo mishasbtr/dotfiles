56318963ed056c672f15779f5f30c1b3f6aeecc5
Mykhailo Sabatura <mishasbtr@gmail.com>
2025-11-21 06:19:33 -0600

    fix(nvim): fix blameline

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -1,13 +1,13 @@
 return {
   {
     "ellisonleao/gruvbox.nvim",
-    priority = 1000,
-    config = true,
-    opts = function(_, opts)
-      opts.context = "hard"
-      opts.transparent_mode = true
-      vim.o.background = "dark"
-    end,
+    opts = {
+      context = "hard",
+      transparent_mode = true,
+      overrides = {
+        GitSignsCurrentLineBlame = { link = "GruvboxBlue" },
+      },
+    },
   },
   {
     "LazyVim/LazyVim",
--- config/nvim/lua/plugins/gitsigns.lua
+++ config/nvim/lua/plugins/gitsigns.lua
@@ -4,11 +4,8 @@ return {
     current_line_blame = true,
     current_line_blame_opts = {
       virt_text = true,
-      virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+      virt_text_pos = "right_align", -- 'eol' | 'overlay' | 'right_align'
       delay = 100,
-      ignore_whitespace = false,
-      virt_text_priority = 100,
-      use_focus = true,
     },
     current_line_blame_formatter = "<author>, <author_time:%R> - <summary>",
   },
